cb1da840e528a34bc13a86991b4f99340542feff
Sergio Roncancio <sfroncancio71@ucatolica.edu.co>
2024-03-08 23:44:10 -0500

    Acceso sin autenticación

--- src/lowleveldesign/DesignVendingMachine/Main.java
+++ src/lowleveldesign/DesignVendingMachine/Main.java
@@ -7,67 +7,61 @@ import lowleveldesign.DesignVendingMachine.VendingStates.State;
 import lowleveldesign.DesignVendingMachine.VendingStates.impl.AuthenticationOption;
 import lowleveldesign.DesignVendingMachine.VendingStates.impl.AuthenticatorFactory;
 
-
 public class Main {
 
     public static void main(String[] args) {
-    VendingMachine vendingMachine = new VendingMachine();
-    boolean authenticationSuccessful = false;
-    int authenticationAttempts = 0;
+        VendingMachine vendingMachine = new VendingMachine();
 
-    // Llena el inventario y muestra los productos disponibles
-    System.out.println("| Filling up the inventory |");
-    fillUpInventory(vendingMachine);
-    displayInventory(vendingMachine);
+        boolean isGuest = chooseUserType(); 
 
-    // Solicita al usuario que elija un método de autenticación
-    AuthenticationOption authenticationOption = chooseAuthenticationMethod();
-    Authenticator authenticator = AuthenticatorFactory.getAuthenticator(authenticationOption);
+        boolean authenticationSuccessful = false;
+        if (!isGuest) {
+            authenticationSuccessful = authenticate(vendingMachine);
+        }
 
-    while (!authenticationSuccessful && authenticationAttempts < 3) {
-        try {
-            // Realiza la autenticación según el método elegido
-            authenticationSuccessful = authenticator.authenticate(vendingMachine);
-
-            // Incrementa el contador de intentos de autenticación
-            authenticationAttempts++;
-            
-            if (!authenticationSuccessful && authenticationAttempts < 3) {
-                System.out.println("Autenticación fallida. Por favor, intente nuevamente.");
-            }
-        } catch (Exception e) {
-            // Manejar cualquier excepción que ocurra durante el proceso de autenticación
-            System.out.println("Se produjo un error durante la autenticación. Por favor, intente nuevamente.");
+        if (isGuest || authenticationSuccessful) {
+            displayOptions(isGuest);
+        } else {
+            System.out.println("Número máximo de intentos de autenticación alcanzado, usuario bloqueado. Contacte al administrador. Finalizando...");
+            System.exit(0);
         }
     }
 
-    if (authenticationSuccessful) {
-          try {
-             Scanner scanner = new Scanner(System.in);
-            System.out.println("¡Autenticación exitosa! Opciones restringidas:");
-            System.out.println("1. Gestionar inventario");
-            System.out.println("2. Gestionar monedas o billetes");
-            System.out.println("Ingrese el número de la opción deseada:");
-            int option = scanner.nextInt();
-            switch (option) {
-                case 1:
-                manageInventory();
-                break;
-             case 2:
-                manageCoinsOrBills();
-                break;
-                default:
-                System.out.println("Opción no válida");
-                break;
-            }
-        } catch (Exception e) {
-            displayInventory(vendingMachine);
-        }
-    } else {
-        System.out.println("Número máximo de intentos de autenticación alcanzado, usuario bloqueado. Contacte al administrador. Finalizando...");
-        System.exit(0); 
+    private static boolean chooseUserType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Desea acceder como invitado?");
+        System.out.println("1. Sí");
+        System.out.println("2. No");
+        int choice = scanner.nextInt();
+        return choice == 1;
     }
-}
+
+    private static boolean authenticate(VendingMachine vendingMachine) {
+        boolean authenticationSuccessful = false;
+        int authenticationAttempts = 0;
+
+        System.out.println("| Filling up the inventory |");
+        fillUpInventory(vendingMachine);
+        displayInventory(vendingMachine);
+
+        AuthenticationOption authenticationOption = chooseAuthenticationMethod();
+        Authenticator authenticator = AuthenticatorFactory.getAuthenticator(authenticationOption);
+
+        while (!authenticationSuccessful && authenticationAttempts < 3) {
+            try {
+                authenticationSuccessful = authenticator.authenticate(vendingMachine);
+                authenticationAttempts++;
+
+                if (!authenticationSuccessful && authenticationAttempts < 3) {
+                    System.out.println("Autenticación fallida. Por favor, intente nuevamente.");
+                }
+            } catch (Exception e) {
+                System.out.println("Se produjo un error durante la autenticación. Por favor, intente nuevamente.");
+            }
+        }
+
+        return authenticationSuccessful;
+    }
 
     private static AuthenticationOption chooseAuthenticationMethod() {
         Scanner scanner = new Scanner(System.in);
@@ -81,18 +75,87 @@ public class Main {
                 return AuthenticationOption.UserCREDENTIALS;
             case 2:
                 System.out.println("PIN authentication is not available at the moment.");
-            return AuthenticationOption.UserCREDENTIALS; 
+                return AuthenticationOption.UserCREDENTIALS;
             case 3:
                 System.out.println("Social Media authentication is not available at the moment.");
-            return AuthenticationOption.UserCREDENTIALS; 
+                return AuthenticationOption.UserCREDENTIALS;
             default:
                 throw new IllegalArgumentException("Invalid choice");
         }
     }
 
     
+
+    private static void displayOptions(boolean isGuest) {
+        if (isGuest) {
+            VendingMachine vendingMachine = new VendingMachine();
+                 try {
+
+            System.out.println("|");
+            System.out.println("filling up the inventory");
+            System.out.println("|");
+
+            fillUpInventory(vendingMachine);
+            displayInventory(vendingMachine);
+
+            System.out.println("|");
+            System.out.println("clicking on InsertCoinButton");
+            System.out.println("|");
+
+            State vendingState = vendingMachine.getVendingMachineState();
+            vendingState.clickOnInsertCoinButton(vendingMachine);
+
+            vendingState = vendingMachine.getVendingMachineState();
+            vendingState.insertCoin(vendingMachine, Coin.NICKEL);
+            vendingState.insertCoin(vendingMachine, Coin.QUARTER);
+           // vendingState.insertCoin(vendingMachine, Coin.NICKEL);
+
+            System.out.println("|");
+            System.out.println("clicking on ProductSelectionButton");
+            System.out.println("|");
+            vendingState.clickOnStartProductSelectionButton(vendingMachine);
+
+            vendingState = vendingMachine.getVendingMachineState();
+            vendingState.chooseProduct(vendingMachine, 102);
+
+            displayInventory(vendingMachine);
+
+        }
+        catch (Exception e){
+            displayInventory(vendingMachine);
+        }
+        } else {
+            System.out.println("¡Autenticación exitosa! Opciones restringidas:");
+            System.out.println("1. Gestionar inventario");
+            System.out.println("2. Gestionar monedas o billetes");
+            System.out.println("Ingrese el número de la opción deseada:");
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    manageInventory();
+                    break;
+                case 2:
+                    manageCoinsOrBills();
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }
+    }
+
    
-    private static void fillUpInventory(VendingMachine vendingMachine){
+
+    private static void manageInventory() {
+        System.out.println("Gestionando el inventario...");
+    }
+
+    private static void manageCoinsOrBills() {
+        System.out.println("Gestionando las monedas o billetes...");
+    }
+    
+        private static void fillUpInventory(VendingMachine vendingMachine){
         ItemShelf[] slots = vendingMachine.getInventory().getInventory();
         for (int i = 0; i < slots.length; i++) {
             Item newItem = new Item();
@@ -126,12 +189,5 @@ public class Main {
         }
     }
     
-    private static void manageInventory() {
-        System.out.println("Gestionando el inventario...");
-    }
-
-    private static void manageCoinsOrBills() {
-        System.out.println("Gestionando las monedas o billetes...");
-    }
     
 }
